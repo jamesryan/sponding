254f9934ee1ae92b6f03c4a6387d4ea6116270b9
James Singley <james.singley@ariix.com>
2016-12-19 08:20:52 -0700

    changed layout and logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {ROUTES} from './app.routes';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(ROUTES),
     HttpModule
   ],
   providers: [],
