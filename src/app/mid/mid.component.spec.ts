407dbd81af45edf1997164c380a0c8bd953dd8e0
James Singley <james.singley@ariix.com>
2017-01-06 15:47:32 -0700

    updated images and added midcomponent

---
+++ src/app/mid/mid.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { MidComponent } from './mid.component';
+
+describe('Component: Mid', () => {
+  it('should create an instance', () => {
+    let component = new MidComponent();
+    expect(component).toBeTruthy();
+  });
+});
